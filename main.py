d732651cf9068fd60c6cf2dbae4a03c9c207f151
milad.saedibaghjeghaz <milad.saedibaghjeghaz@quandoo.com>
2023-06-01 13:55:07 +0200

    add/update words into module openDict

---
+++ main.py
@@ -0,0 +1,10 @@
+import openDict
+
+
+db = {"Milad": ["reborn"]}
+nw = "Milad"
+mn = "son of sun"
+
+print(db)
+openDict.add_word(db, nw, mn)
+print(db)
---
+++ openDict.py
@@ -0,0 +1,25 @@
+
+"""
+This module supposed to add/update/delete words and it's meaning to a datatype
+it should be able to write it to a file
+"""
+
+
+# add new world with meaning into db or update it if key exists
+def add_word(dbdict, new_word, meaning):
+    mydict = dbdict
+    # if this word does not exist in the dictionary:
+    # then add it + it's meaning
+    if new_word in dbdict:
+        print("the key exists in db, let's update meaning")
+        current_meaning = dbdict.get(new_word)
+        current_meaning.append(meaning)
+        dbdict[new_word]=current_meaning
+    # if it doesn't exist then let's add it in the meaning lists
+    else:
+        print("key does not exists in db, let's add it")
+        mydict[new_word]= [meaning]
+
+
+# Todo: need a function to write changes into a file in filesystem
+
