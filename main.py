f2f0d9089354df48047ec9569bef25ed0affeb56
milad.saedibaghjeghaz <milad.saedibaghjeghaz@quandoo.com>
2023-06-03 18:52:10 +0200

    load sample data into dictionary

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import openDict
 
-db_dict = openDict.OpenDict("a.txt")
+db_dict = openDict.OpenDict("a.txt", True)
 db_dict.show_data()
-
-db_dict.add_word("m4", "e4")
-db_dict.show_data()
+#
+# db_dict.add_word("m4", "e4")
+# db_dict.show_data()
+# db_dict.delete_word("k1")
+# db_dict.show_data()
--- openDict.py
+++ openDict.py
@@ -1,12 +1,30 @@
+import nltk
+from nltk.corpus import words, wordnet
+import random
+
+
 class OpenDict:
 
     data = {}
     filename = ""
 
-    def __init__(self, filename):
+    def __init__(self, filename, is_load_random=False):
         self.filename = filename
-        with open(filename, 'r') as file:
-            self.data = eval(file.read())
+        if not is_load_random:
+            # load DB with random words and their meaning
+            with open(filename, 'r') as file:
+                self.data = eval(file.read())
+        else:
+            # Download required data
+            nltk.download('wordnet')
+            nltk.download('words')
+
+            word_list = words.words()
+            for wrd in range(100):
+                random_word = random.choice(word_list)
+                # add word and meanings into db
+                meanings = wordnet.synsets("word")
+                self.add_word(random_word,meanings[0].definition())
 
     def show_data(self):
         print(self.data)
@@ -16,24 +34,43 @@ class OpenDict:
         with open(self.filename, 'w') as file:
             file.write(str(self.data))
 
+    # add/update a word into dictionary
     def add_word(self, new_word, meaning):
         if new_word in self.data:
-            print("the key exists in db, let's update meaning")
+            # the key exists in db, let's update meaning"
             current_meaning = self.data.get(new_word)
+            if meaning in current_meaning:
+                # no need to add , it is already in db
+                return
             current_meaning.append(meaning)
             self.data[new_word] = current_meaning
             self.save_data(self)
         # if it doesn't exist then let's add it in the meaning lists
         else:
-            print("key does not exists in db, let's add it")
+            # the key does not exist in db, let's add it
             self.data[new_word] = [meaning]
             self.save_data(self)
 
+    # delete a word from dictionary
+    def delete_word(self, word):
+        if word in self.data.keys():
+            self.data.pop(word)
+            self.save_data(self)
+        return f"{word} does not exists in dictionary"
 
-# Todo: need a function to write changes into a file in filesystem
-# def save_to_file(path, filename, dict)
 
-# todo: delete words(key) if exists in my dictionary
-# def delete_word(dict, word)
+
 
 # todo: different init based on database { file | postgresql )
+
+# todo: a function to search a word in db and return it's values
+
+# todo: if a searched word, does not exist in db, then use a 3rd party app
+#  to get the meaning and add it to the database and also return to the user
+
+# todo: we need the capability of speech in our application :)
+
+# todo: remove the words that has no meaning in my dictionary
+
+# todo: improve show_data function
+
