5dfa8585bd1929760e95d2de7acc1e399f1546d2
Ali <ali.moayer5879@gmail.com>
2023-06-03 20:06:50 +0300

    Add Class Dict includ 3 funvtion:Write,update & Delete & saving file. also add db.txt for file Database

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import openDict
 
-
 db = {"Milad": ["reborn"]}
-nw = "Milad"
+nw = "Reza"
 mn = "son of sun"
 
 print(db)
-openDict.add_word(db, nw, mn)
+my_dict = openDict.Dict()
+my_dict.add_word(db, nw, mn)
 print(db)
+
+my_dict.save_to_file('db.txt', db)
+
+my_dict.delete_word(db, 'Ali')
+print(db)
+
+my_dict.save_to_file('db.txt', db)
+
--- openDict.py
+++ openDict.py
@@ -1,30 +1,50 @@
-
 """
 This module supposed to add/update/delete words and it's meaning to a datatype
 it should be able to write it to a file
 """
 
-
-# add new world with meaning into db or update it if key exists
-def add_word(dbdict, new_word, meaning):
-    mydict = dbdict
-    # if this word does not exist in the dictionary:
-    # then add it + it's meaning
-    if new_word in dbdict:
-        print("the key exists in db, let's update meaning")
-        current_meaning = dbdict.get(new_word)
-        current_meaning.append(meaning)
-        dbdict[new_word] = current_meaning
-    # if it doesn't exist then let's add it in the meaning lists
-    else:
-        print("key does not exists in db, let's add it")
-        mydict[new_word] = [meaning]
+import os
 
 
-# Todo: need a function to write changes into a file in filesystem
-# def save_to_file(path, filename, dict)
+class Dict:
+    def __init__(self):
+        self.filename = None
+        self.word = None
+        self.dbdict = None
+        self.meaning = None
+        self.new_word = None
 
-# todo: delete words(key) if exists in my dictionary
-# def delete_word(dict, word)
+    def add_word(self, dbdict, new_word, meaning):
+        self.dbdict = dbdict
+        self.new_word = new_word
+        self.meaning = meaning
+        mydict = self.dbdict
+        if self.new_word in self.dbdict:
+            print(f"{self.new_word} exists in db, let's update meaning")
+            current_meaning = self.dbdict.get(self.new_word)
+            current_meaning.append(self.meaning)
+            self.dbdict[self.new_word] = current_meaning
+        else:
+            print(f"{self.new_word} does not exists in db, let's add it")
+            mydict[self.new_word] = [self.meaning]
 
-# todo: Implement through OOP
+    # todo: delete words(key) if exists in my dictionary
+    def delete_word(self, dbdict, word):
+        self.dbdict = dbdict
+        self.word = word
+        mydict = self.dbdict
+        if self.word in mydict:
+            del mydict[self.word]
+            print(f"Deleted {self.word} in dictionary")
+        else:
+            print(f"There isn't {self.word} in Dictionary for delete")
+
+    # Todo: need a function to write changes into a file in filesystem
+    def save_to_file(self, filename, dbdict):
+        self.filename = filename
+        self.dbdict = dbdict
+        path = os.getcwd() + "/" + self.filename
+        my_file = open(path, 'w')
+        my_file.write(str(self.dbdict))
+        my_file.close()
+
