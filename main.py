629ed377fee4cf8730cddea95c3387d31c66aa05
milad.saedibaghjeghaz <milad.saedibaghjeghaz@quandoo.com>
2023-06-04 11:43:27 +0200

    fix protected methods

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import openDict
 
 db_dict = openDict.OpenDict("a.txt", True)
 db_dict.show_data()
-#
-# db_dict.add_word("m4", "e4")
-# db_dict.show_data()
-# db_dict.delete_word("k1")
-# db_dict.show_data()
+db_dict.__data = {}
+db_dict.show_data()
--- openDict.py
+++ openDict.py
@@ -5,7 +5,7 @@ import random
 
 class OpenDict:
 
-    data = {}
+    __data = {}
     filename = ""
 
     def __init__(self, filename, is_load_random=False):
@@ -27,35 +27,34 @@ class OpenDict:
                 self.add_word(random_word,meanings[0].definition())
 
     def show_data(self):
-        print(self.data)
+        print(self.__data)
 
-    @staticmethod
-    def save_data(self):
+    def __save_data(self):
         with open(self.filename, 'w') as file:
-            file.write(str(self.data))
+            file.write(str(self.__data))
 
     # add/update a word into dictionary
     def add_word(self, new_word, meaning):
-        if new_word in self.data:
+        if new_word in self.__data:
             # the key exists in db, let's update meaning"
-            current_meaning = self.data.get(new_word)
+            current_meaning = self.__data.get(new_word)
             if meaning in current_meaning:
                 # no need to add , it is already in db
                 return
             current_meaning.append(meaning)
-            self.data[new_word] = current_meaning
-            self.save_data(self)
+            self.__data[new_word] = current_meaning
+            self.__save_data()
         # if it doesn't exist then let's add it in the meaning lists
         else:
             # the key does not exist in db, let's add it
-            self.data[new_word] = [meaning]
-            self.save_data(self)
+            self.__data[new_word] = [meaning]
+            self.__save_data()
 
     # delete a word from dictionary
     def delete_word(self, word):
         if word in self.data.keys():
             self.data.pop(word)
-            self.save_data(self)
+            self.__save_data()
         return f"{word} does not exists in dictionary"
 
 
@@ -69,8 +68,6 @@ class OpenDict:
 #  to get the meaning and add it to the database and also return to the user
 
 # todo: we need the capability of speech in our application :)
-
-# todo: remove the words that has no meaning in my dictionary
 
 # todo: improve show_data function
 
