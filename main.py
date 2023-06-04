690b7232aa4f2c2b8cc61c1145172640a3fdcbc3
milad.saedibaghjeghaz <milad.saedibaghjeghaz@quandoo.com>
2023-06-04 20:55:46 +0200

    fix

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import openDict
 
-db_dict = openDict.OpenDict("a.txt", False)
+db_dict = openDict.OpenDict("a.txt", True)
 db_dict.show_data()
 db_dict.__data = {}
 db_dict.search_data('torturously')
 db_dict.delete_word("calciphilia")
 db_dict.add_word('calciphilia', 'this is test')
 db_dict.add_word('calciphilia', 'test3')
+db_dict.show_data()
