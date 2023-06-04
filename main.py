26d84257288346f1c685ce64390cadaee7bf246e
Ali <ali.moayer5879@gmail.com>
2023-06-04 17:49:15 +0300

    Add Search function & improve show_data function & other prints

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import openDict
 
-db_dict = openDict.OpenDict("a.txt", True)
+db_dict = openDict.OpenDict("a.txt", False)
 db_dict.show_data()
 db_dict.__data = {}
-db_dict.show_data()
+db_dict.search_data('torturously')
+db_dict.delete_word("calciphilia")
+db_dict.add_word('calciphilia', 'this is test')
+db_dict.add_word('calciphilia', 'test3')
